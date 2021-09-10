002b4195cee1dbd73d5c606ddad0c4ece5ffb41b
dAxeponb <dragonxxl2014@gmail.com>
2021-09-10 18:55:44 +0700

    Add documentation for min and max methods

--- app/src/main/java/ru/kapmax/lab1/TestsMethods.java
+++ app/src/main/java/ru/kapmax/lab1/TestsMethods.java
@@ -8,7 +8,8 @@ package ru.kapmax.lab1;
 class TestsMethods {
     /**
      * Метод для нахождения максимума между двумя целыми числами
-     * @param int a, int b
+     * @param a целое число
+     * @param b целое число
      * @return Максимум между a и b
      */
     public static int max(int a, int b) {
@@ -18,7 +19,8 @@ class TestsMethods {
 
     /**
      * Метод для нахождения минимума между двумя целыми числами
-     * @param int a, int b
+     * @param a целое число
+     * @param b целое число
      * @return Минимум между a и b
      */
     public static int min(int a, int b) {
